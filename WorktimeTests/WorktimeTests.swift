80e890c1499292f542e836b16abe2a22cbdd15a4
Ulrich Eitschberger <ulrich.eitschberger@tu-dortmund.de>
2017-05-04 23:23:02 +0200

    Initial Commit

---
+++ WorktimeTests/WorktimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  WorktimeTests.swift
+//  WorktimeTests
+//
+//  Created by Ulrich Eitschberger on 04.05.17.
+//  Copyright © 2017 Ulrich Eitschberger. All rights reserved.
+//
+
+import XCTest
+@testable import Worktime
+
+class WorktimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
