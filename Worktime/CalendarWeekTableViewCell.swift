4de363f8198644557eba547668490dfc5e68af8f
Ulrich Eitschberger <ulrich.eitschberger@tu-dortmund.de>
2017-05-05 16:27:22 +0200

    First steps on the way to a working app

---
+++ Worktime/CalendarWeekTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CalendarWeekTableViewCell.swift
+//  Worktime
+//
+//  Created by Ulrich Eitschberger on 05.05.17.
+//  Copyright © 2017 Ulrich Eitschberger. All rights reserved.
+//
+
+import UIKit
+
+class CalendarWeekTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
